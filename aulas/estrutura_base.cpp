8bbe9a10ce008949cde7e2e57c58dc00f77e898c
karlloscosta <karllos.costa@alunos.ufersa.edu.br>
2024-03-08 02:42:33 -0300

    aula_1

---
+++ aulas/estrutura_base.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // biblioteca de comando básicos do c++
+
+using namespace std; // para indicar onde o c++ vai buscar as bibliotecas
+// função principal
+int main(){
+
+    cout << "Hello, wolrd" << endl; // quebra de linha com endl, também é possível usar \n
+    cout << "Hello, wolrd\nQuebra de linha\n"; // quebra de linha com /n
+    system("pause"); // o sistema vai aguarda uma tecla ser pressionada para prosseguir para o fim da execução
+    return 0;
+}
